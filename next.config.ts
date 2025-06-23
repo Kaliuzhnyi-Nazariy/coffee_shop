0ac02873ea4795a22d15292fed290565b5a38388
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-06-23 14:20:35 +0300

    next config update

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: [
+      "cdn.jsdelivr.net",
+      "avatars.githubusercontent.com",
+      "picsum.photos",
+    ],
+  },
 };
 
 export default nextConfig;
