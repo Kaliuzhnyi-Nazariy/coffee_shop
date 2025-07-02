0c7f6b9a616ea8de0c74a012ce1ab14c6943ca3d
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-07-02 22:55:11 +0300

    change menu appearance

--- src/app/components/home/menu/Menu.tsx
+++ src/app/components/home/menu/Menu.tsx
@@ -3,8 +3,6 @@ import { IoIosMenu } from "react-icons/io";
 import MenuOpen from "./MenuOpen";
 import { useEffect, useState } from "react";
 import { usePathname } from "next/navigation";
-import { useSelector } from "react-redux";
-import { userIsLoggedIn } from "@/app/redux/user/selectors";
 
 const Menu = () => {
   const pathname = usePathname();
@@ -13,6 +11,8 @@ const Menu = () => {
     const link = pathname.slice(1, pathname.length);
 
     if (link === "home") return "";
+    if (link === "auth/signin") return "";
+    if (link === "auth/signup") return "";
     if (link === "our-products") return "Product";
   };
 
@@ -26,22 +26,28 @@ const Menu = () => {
     setMenuOpen(false);
   };
 
+  const isShown = () => {
+    const link = pathname.slice(1, pathname.length);
+
+    if (link === "auth/signin") {
+      return "hidden";
+    } else if (link === "auth/signup") {
+      return "hidden";
+    } else {
+      return "";
+    }
+  };
+
   const menuStyles = `absolute top-5 right-[25px] w-[25px] h-[25px] flex justify-center items-center z-[2] ${
     title() === "" ? "text-white" : "text-black"
-  }`;
+  } ${isShown()} `;
 
   useEffect(() => {
     document.body.style.overflow = isMenuOpen ? "hidden" : "";
   }, [isMenuOpen]);
 
-  const isShown = useSelector(userIsLoggedIn);
-
   return (
-    <div
-      className={`absolute z-10 w-full h-[66px] ${
-        isShown ? "block" : "hidden"
-      } `}
-    >
+    <div className={`absolute z-10 w-full h-[66px]  `}>
       <h2 className="flex items-center h-[66px] ml-5 text-[16px]">{title()}</h2>
       <button className={menuStyles} onClick={onOpen}>
         <IoIosMenu />
