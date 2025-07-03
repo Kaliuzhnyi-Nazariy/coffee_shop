ef0b06e693fa33e665832e2d8b3700bd5f1b83f2
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-07-03 22:44:03 +0300

    wishlist

---
+++ src/app/wishlist/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import WishlistComponent from "../components/wishlist";
+
+const pages = () => {
+  return (
+    <div className="flex-grow flex justify-center items-center">
+      <WishlistComponent />
+    </div>
+  );
+};
+
+export default pages;
